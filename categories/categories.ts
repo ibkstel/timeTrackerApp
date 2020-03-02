332e30a46bf7b886651235650f086f71825df982
Selim <selim.ustel1998@gmail.com>
2020-03-02 17:36:01 +0300

    basics of categories are added to branch

---
+++ categories/categories.ts
@@ -0,0 +1,37 @@
+interface category {
+    name:string,
+    icon:string,
+}
+
+export const categories: Array<category> = [
+    {
+
+        name:"Transportation",
+        icon: "",
+
+
+    },
+    {
+        name:"Social",
+        icon:"",
+
+    },
+    {
+        name:"Gym",
+        icon:"",
+
+    },
+    {
+        name:"School",
+        icon:"",
+
+    },
+    {
+        name:"School",
+        icon:"",
+    }
+
+
+
+
+];
